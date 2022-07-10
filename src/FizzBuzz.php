d05999b6fe6443ae348437e2963d1b33ebb0f80f
Viviana <vivitr5195@gmail.com>
2022-07-11 00:15:11 +0200

    exercise php

---
+++ src/FizzBuzz.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+class FizzBuzz{
+public function isDivisible(int $numb):string
+{
+    if($numb % 3 === 0 && $numb % 5 === 0){
+        return "FizzBuzz";
+    }
+if($numb % 3 === 0){
+    return "Fizz";
+}
+if($numb % 5 === 0){
+    return "Buzz";
+}
+//return ($numb % 3 === 0 && $numb % 5 === 0? "FizzBuzz");
+return $numb;
+}
+}
